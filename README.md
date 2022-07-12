c90ea4fe907fcff699b9403a5d92ca1b3da9a008
Josh Shearer <joshshear@gmail.com>
2022-07-11 20:55:09 -0700

    Base Build

---
+++ README.md
@@ -0,0 +1,28 @@
+# CGM Data Processing - Sleep, Diet, Exercise
+This script will take in and process data from the following sources
+
+```yaml
+xDrip+ => Continuous Glucose Monitoring Data
+Oura = Sleep Data
+Cronometer = Food Intake and suppliment consumption
+Garmin = Exercise and training data
+```
+
+## Setup 
+
+To run the bot, open the command line in the cloned repo directory and install the requirements using pip with the following command:
+```bash
+pip install -r requirements.txt
+```
+
+
+
+
+## Execute
+
+To run the bot, run the following in the command line:
+```
+python3 main.py
+```
+
+
---
+++ cgmprocessing.py
@@ -0,0 +1,18 @@
+import glob
+import pandas as pd
+import numpy as np
+import re
+from datetime import date, time, datetime, timedelta
+from time import sleep
+from matplotlib import pyplot as plt
+import itertools
+from bokeh.io import curdoc, show
+from bokeh.models import Label, Legend, LegendItem, LabelSet, Span, BoxAnnotation, ColumnDataSource, ColorBar, BasicTicker,  PrintfTickFormatter, LinearColorMapper, Range1d, Grid, LinearAxis, MultiLine, Plot, layouts
+from bokeh.plotting import figure, output_file, show
+from bokeh.sampledata.les_mis import data
+from bokeh.transform import transform
+
+
+class CGMProcessing:
+    def __init__(self):
+
